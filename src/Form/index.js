859a225cad0684269f8a1399397daa3667c4dbb8
Bartek Pawlaczyk <bpawlaczyk1982@gmail.com>
2023-06-06 22:19:17 +0200

    deleted not used favicon.ico and fixed autoFocus in form

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Form = () => (
     <form className="form">
-        <input autofocus placeholder="Co jest do zrobienia?" className="form__input" />
+        <input autoFocus placeholder="Co jest do zrobienia?" className="form__input" />
         <button className="form__button">Dodaj zadanie</button>
     </form>
 );
