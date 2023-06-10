58a8b38a754a73f518338d9ceada6d17042663db
Bartek Pawlaczyk <bpawlaczyk1982@gmail.com>
2023-06-10 14:50:07 +0200

    changed index.js in Tasks using destructurisation

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,22 +1,21 @@
 import "./style.css";
 
-const Tasks = (props) => (
-    <ul className="taskList">
-        {props.tasks.map(task => (
-            <li
-                className={`taskList__item ${task.done && props.hideDoneTasks ? "taskList__item--hidden" : ""}`}
-            >
-                <button className="taskList__button taskList__button--finished">
-                    {task.done ? "✓" : ""}
-                </button>
-                <span className={`taskList__content ${task.done ? "taskList__content--finished" : ""}`}>
-            {task.content}
-          </span>
-          <button className="taskList__button taskList__button--delete">🗑️
-          </button>
-                </li>
-            ))}
-        </ul>
-    );
+const Tasks = ({ tasks, hideDoneTasks }) => (
+  <ul className="taskList">
+    {tasks.map((task) => (
+      <li
+        className={`taskList__item ${task.done && hideDoneTasks ? "taskList__item--hidden" : ""}`}
+      >
+        <button className="taskList__button taskList__button--finished">
+          {task.done ? "✓" : ""}
+        </button>
+        <span className={`taskList__content ${task.done ? "taskList__content--finished" : ""}`}>
+          {task.content}
+        </span>
+        <button className="taskList__button taskList__button--delete">🗑️</button>
+      </li>
+    ))}
+  </ul>
+);
 
-    export default Tasks;
+export default Tasks;
